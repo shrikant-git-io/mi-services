aa9cea4fa4750d9f4058098b1a38aa57a0638f88
shrikant-git-io <shrikantkumkar27@gmail.com>
2020-01-10 09:14:40 +0530

    zuul chnages

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -6,13 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange-service" , url = "http://localhost:8000" )
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service" , url = "http://localhost:8000" ) - direct call
+//@FeignClient(name = "currency-exchange-service") - using eureka service name
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 //client side load balancing , check app properties for ports configured, in case of eureka, it identifies based on service name
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //@GetMapping("/currency-exchange/from/{from}/to/{to}") - using eureka service name
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retriveExchangeValue(@PathVariable String from, @PathVariable String to);
 
     }
