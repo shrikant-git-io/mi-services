31aa370ce1298a327973158fc213c138152b750d
shrikant-git-io <shrikantkumkar27@gmail.com>
2020-03-29 20:26:15 +0530

    end to end changes

--- limits-service/src/main/java/com/microservices/limitsservice/controller/LimitsConfigController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/controller/LimitsConfigController.java
@@ -2,9 +2,13 @@ package com.microservices.limitsservice.controller;
 
 import com.microservices.limitsservice.bean.LimitConfiguration;
 import com.microservices.limitsservice.config.LimitsConfig;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.HystrixCommands;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.*;
 
 @RestController
 public class LimitsConfigController {
@@ -17,5 +21,15 @@ public class LimitsConfigController {
     public LimitConfiguration retriveLimitsFromConfig(){
 
        return new LimitConfiguration(limitsConfig.getMinimum(),limitsConfig.getMaximum());
+   }
+
+   @GetMapping("/faultTolerenceExample")
+   @HystrixCommand(fallbackMethod = "retriveConfigFallback")
+    public LimitConfiguration retriveConfig(){
+       throw new RuntimeException();
+   }
+
+   public LimitConfiguration retriveConfigFallback(){
+       return new LimitConfiguration(100,1000);
    }
 }
